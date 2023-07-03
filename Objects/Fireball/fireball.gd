23b3d9e2245a6b39059355342c15f653cc46f563
Széri József <32771024+UltraMKF12@users.noreply.github.com>
2023-07-03 16:06:43 +0300

    Made view biggeer, fireball collison, player camera

--- Objects/Fireball/fireball.gd
+++ Objects/Fireball/fireball.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var pixel_per_second: int = 100
+var pixel_per_second: int = 150
 var direction: Vector2
 
 func _ready():
@@ -9,6 +9,9 @@ func _ready():
 func _process(delta):
 	move_and_slide()
 	
+	# If it collides, it must be an enemy or a wall
+	if get_slide_collision_count() > 0:
+		queue_free()
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
