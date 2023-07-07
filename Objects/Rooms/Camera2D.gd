1f0d3b3495c9b8296131a134ba60b6916c4e817d
AA6363 <nemestamas30@gmail.com>
2023-07-07 14:29:30 +0200

    Implements camera fixes
    -Camera now follows the player and remains at the spot, where the player dies after the player has been killed.

--- Objects/Rooms/Camera2D.gd
+++ Objects/Rooms/Camera2D.gd
@@ -1,6 +1,8 @@
 extends Camera2D
 
+@onready var player: Node2D = $"../Player"
 
+var pixel_per_second: int = 100
 
 func _ready():
 	pass 
@@ -8,4 +10,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = position.direction_to($"../Player".position)
+	if get_parent().has_node("Player"):
+		position = player.position
+		
