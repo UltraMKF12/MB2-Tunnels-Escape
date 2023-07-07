f7287f6ffcb01aa3e5db99727ab2a48c5a9944c2
Széri József <32771024+UltraMKF12@users.noreply.github.com>
2023-07-07 15:38:03 +0300

    Camera script fix
    +Put the script in physics_process because the player moves in physics process. Otherwise the player will become shaky because the camera updates too fast/too slow
    
    +Removed the get_parent().get_node() test, as it's simply unnecesary to check for the existance of a node every single frame, because we already have a reference for it. We simply need to check if it's valid.

--- Objects/Rooms/Camera2D.gd
+++ Objects/Rooms/Camera2D.gd
@@ -2,14 +2,11 @@ extends Camera2D
 
 @onready var player: Node2D = $"../Player"
 
-var pixel_per_second: int = 100
 
 func _ready():
 	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if get_parent().has_node("Player"):
+ 
+func _physics_process(delta):
+	if is_instance_valid(player):
 		position = player.position
-		
